486538617c30c24898051aa29d3fe85fb81c71d6
Eric Joshua Parish <ejparis@sandia.gov>
2025-07-17 16:28:37 -0500

    trying yet again

--- norma-regression-tests/single/cuboid/dynamic-opinf-fom/check_models.py
+++ norma-regression-tests/single/cuboid/dynamic-opinf-fom/check_models.py
@@ -4,4 +4,4 @@ data = np.load('opinf-operator-gold.npz')
 data2  = np.load('opinf-operator.npz')
 
 for key in list(data.keys()):
-  assert np.allclose(data[key],data2[key])
+  assert np.allclose(data[key],data2[key]), print(key,data2[key])
