e2922703a13b2c55d50b724dda3f1b664f06c9af
Eric Joshua Parish <ejparis@sandia.gov>
2025-07-17 13:26:39 -0500

    adding single opinf

--- norma-regression-tests/single/cuboid/dynamic-opinf-fom/make_opinf_models.py
+++ norma-regression-tests/single/cuboid/dynamic-opinf-fom/make_opinf_models.py
@@ -1,21 +1,19 @@
 import normaopinf
 import normaopinf.opinf
-import nnopinf
-import nnopinf.training
 import os
 import numpy as np
 
 if __name__ == '__main__':
     settings = {}
     settings['fom-yaml-file'] = 'cuboid.yaml'
-    settings['training-data-directories'] = ['/Users/ejparis/Research/ahead/norma-opinf/examples/single/cuboid/dynamic-opinf-fom']
+    settings['training-data-directories'] = [os.getcwd()]
     settings['model-type'] = 'linear'
     settings['stop-training-time'] = 100000.0
     settings['training-skip-steps'] = 1
     settings['forcing'] = False
     settings['truncation-type'] = 'energy'
     settings['boundary-truncation-type'] = 'energy'
-    settings['regularization-parameter'] = [0.1, 0.2, 0.3]
+    settings['regularization-parameter'] = 'automatic' 
     settings['model-name'] = 'opinf-operator'
     settings['truncation-value'] = 0.999999
     settings['boundary-truncation-value'] = 0.999999
